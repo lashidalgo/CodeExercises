c341f6d4602a96656c18ca53313628f64e2e8f67
lashidalgo <lashidalgo@gmail.com>
2015-06-02 12:18:47 -0400

    -Added Make it anagram -Added Panagram

---
+++ Make it Anagram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Make_it_Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var asciiListFirst = Console.ReadLine().ToCharArray();
+                var asciiListSecond = Console.ReadLine().ToCharArray();
+
+                var diffs = new int[26];
+
+                foreach (var character in asciiListFirst)
+                {
+                    diffs[character - 'a']++;
+                }
+
+                foreach (var character in asciiListSecond)
+                {
+                    diffs[character - 'a']--;
+                }
+
+                Console.WriteLine(diffs.Sum(diff => Math.Abs(diff)));
+            }
+        }
+    }
+}
---
+++ Panagram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Delete spaces, conver to UpperCase and char array to get the ASCII value
+            Console.WriteLine(IsPanagram(Encoding.ASCII.GetBytes(Console.ReadLine().Replace(" ","").ToUpper().ToCharArray()).ToList()));
+        }
+
+        //ASCII from [A-Z] ->  [65-90]
+        static string IsPanagram(List<byte> readLine)
+        {
+            readLine = readLine.Distinct().ToList();
+            readLine.Sort();
+
+            if (readLine.Count < 26)
+            {
+                return "not pangram";
+            }
+
+            for (int i = 65; i < 90; i++)
+            {
+                if (!readLine.ElementAt(i - 65).Equals((byte)i))
+                {
+                    return "not pangram";
+                }
+            }
+
+            return "pangram";
+        }
+    }
+}
