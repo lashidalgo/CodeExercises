5ca99dd4767474e9a694d170c2d956c4e167ba99
aorrego <alejandro.orrego@crystaldev.cl>
2015-06-09 23:33:12 -0400

    Valid PAN format

---
+++ Valid PAN format/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Valid_PAN_format
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {          
+            int noTestCases = int.Parse(  Console.ReadLine() );
+            string[] pans = new string[noTestCases];
+            for (int testCase = 0; testCase < noTestCases; testCase++)
+            {
+                pans[testCase] = Console.ReadLine();
+            }
+        
+            processPANs(pans);
+        }
+
+        private static void processPANs(string[] pans)
+        {
+            foreach (string pan in pans)
+            {
+                if (isValidPAN(pan))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+
+        private static bool isValidPAN(string pan)
+        {
+            Regex rPan = new Regex(@"^([A-Z]){5}([0-9]){4}([a-zA-Z]){1}?$");
+
+            if (pan.Length != 10)
+	        {
+                return false;
+	        }
+           
+            return rPan.IsMatch(pan );
+        }
+    }
+}
